425846a62525faa6e6747a0fea9050c6d1e50bc3
namanseth01 <naman.seth_cs19@gla.ac.in>
2021-11-29 23:02:58 +0530

    calling sort method

--- src/SortingAlgorithms/BubbleSort.java
+++ src/SortingAlgorithms/BubbleSort.java
@@ -13,4 +13,20 @@ public class BubbleSort {
             }
         }
     }
+
+    public static void main(String[] args) {
+        BubbleSort sort = new BubbleSort();
+        int[] numbers = new int[]{66, 55, 89, 12, 9, 3, 75};
+        System.out.print("Given array: ");
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+        System.out.println();
+        System.out.println("-----------------------");
+        System.out.println("Sorted Array after applying Bubble Sort: ");
+        sort.sort(numbers);
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+    }
 }
