a3671ddf1fddd10f9468aff1315e24c7f8cdc963
namanseth01 <naman.seth_cs19@gla.ac.in>
2021-11-30 15:10:15 +0530

    add main mthod

--- src/SortingAlgorithms/QuickSort.java
+++ src/SortingAlgorithms/QuickSort.java
@@ -1,7 +1,7 @@
 package SortingAlgorithms;
 
 public class QuickSort {
-    static int partition(int[] arr, int low, int high)
+    public int partition(int[] arr, int low, int high)
     {
         int pivot = arr[high];
         int i = (low - 1);
@@ -22,13 +22,30 @@ public class QuickSort {
         return (i + 1);
     }
 
-    static void sort(int[] arr, int low, int high)
+    public void sort(int[] arr, int low, int high)
     {
         if (low < high)
         {
             int index = partition(arr, low, high);
             sort(arr, low, index - 1);
             sort(arr, index + 1, high);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        QuickSort sort = new QuickSort();
+        int[] numbers = new int[]{66, 55, 89, 12, 9, 3, 75};
+        System.out.print("Given array: ");
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+        System.out.println();
+        System.out.println("-----------------------");
+        System.out.println("Sorted Array after applying Quick Sort: ");
+        sort.sort(numbers, 0 , numbers.length - 1);
+        for (int number : numbers) {
+            System.out.print(number + " ");
         }
     }
 }
