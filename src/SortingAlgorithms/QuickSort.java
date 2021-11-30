1613b904ba2222f9ad4552965b9f37067dc4d5b6
namanseth01 <naman.seth_cs19@gla.ac.in>
2021-11-30 15:08:18 +0530

    add sort method

--- src/SortingAlgorithms/QuickSort.java
+++ src/SortingAlgorithms/QuickSort.java
@@ -1,4 +1,15 @@
 package SortingAlgorithms;
 
 public class QuickSort {
+
+
+    static void sort(int[] arr, int low, int high)
+    {
+        if (low < high)
+        {
+            int index = partition(arr, low, high);
+            sort(arr, low, index - 1);
+            sort(arr, index + 1, high);
+        }
+    }
 }
