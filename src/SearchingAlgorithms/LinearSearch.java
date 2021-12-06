a39994214ee7a97941e3f8d4253baa5a332d3f4d
namanseth01 <naman.seth_cs19@gla.ac.in>
2021-12-06 14:25:00 +0530

    add search method

--- src/SearchingAlgorithms/LinearSearch.java
+++ src/SearchingAlgorithms/LinearSearch.java
@@ -1,4 +1,14 @@
 package SearchingAlgorithms;
 
 public class LinearSearch {
+    public static int search(int arr[], int x)
+    {
+        int n = arr.length;
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] == x)
+                return i;
+        }
+        return -1;
+    }
 }
