ca11c0ccb19cf8e2a7cff017f900daaab46d23ac
namanseth01 <naman.seth_cs19@gla.ac.in>
2021-12-06 14:21:41 +0530

    edit

--- src/SortingAlgorithms/SelectionSort.java
+++ src/SortingAlgorithms/SelectionSort.java
@@ -2,10 +2,10 @@ package SortingAlgorithms;
 
 public class SelectionSort {
 
-    void sort(int arr[])
+    void sort(int[] arr)
     {
         int n = arr.length;
-
+e
         // One by one move boundary of unsorted subarray
         for (int i = 0; i < n-1; i++)
         {
