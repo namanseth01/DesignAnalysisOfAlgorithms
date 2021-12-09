5cdd2f5a0c75e5d7df024e5fe8ede4701a25ed55
namanseth01 <naman.seth_cs19@gla.ac.in>
2021-12-09 21:25:23 +0530

    add FractionalKnapsack class

---
+++ src/GreedyApproach/FractionalKnapsack/FractionalKnapsack.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Naman Seth (namaseth01)
+ *   Date: 09/12/21
+ *   Time: 9:24 PM
+ *   File: FractionalKnapsack.java
+ */
+
+package GreedyApproach.FractionalKnapsack;
+
+public class FractionalKnapsack {
+}
