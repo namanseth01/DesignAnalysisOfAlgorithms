ada680f625ee2958840e4a11a5016a574b720621
namanseth01 <naman.seth_cs19@gla.ac.in>
2021-12-09 21:30:32 +0530

    formatted

--- src/GreedyApproach/FractionalKnapsack/FractionalKnapsack.java
+++ src/GreedyApproach/FractionalKnapsack/FractionalKnapsack.java
@@ -7,13 +7,47 @@
 
 package GreedyApproach.FractionalKnapsack;
 
+import java.util.Arrays;
+
 public class FractionalKnapsack {
 
-    class ItemValue {
+    private static double getMaxValue(int[] wt, int[] val, int capacity)
+    {
+        ItemValue[] iVal = new ItemValue[wt.length];
+
+        for (int i = 0; i < wt.length; i++) {
+            iVal[i] = new ItemValue(wt[i], val[i], i);
+        }
+
+        Arrays.sort(iVal, (o1, o2) -> o2.cost.compareTo(o1.cost));
+
+        double totalValue = 0d;
+
+        for (ItemValue i : iVal) {
+
+            int curWt = (int)i.wt;
+            int curVal = (int)i.val;
+
+            if (capacity - curWt >= 0) {
+                capacity = capacity - curWt;
+                totalValue += curVal;
+            }
+            else {
+                double fraction
+                        = ((double)capacity / (double)curWt);
+                totalValue += (curVal * fraction);
+                capacity = (int)(capacity - (curWt * fraction));
+                break;
+            }
+        }
+
+        return totalValue;
+    }
+
+    static class ItemValue {
         Double cost;
         double wt, val, ind;
 
-        // item value function
         public ItemValue(int wt, int val, int ind)
         {
             this.wt = wt;
